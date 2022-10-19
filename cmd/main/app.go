164e227f45a7694ec06cd9477cd021a6095a9270
Vadim Gerasimov <gerasmeb34@gmail.com>
2022-10-20 02:03:46 +0300

    unfinished version

--- cmd/main/app.go
+++ cmd/main/app.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"net"
 	"net/http"
-	"rest-api/cmd/internal/user"
-	"rest-api/cmd/pkg/logging"
+	"os"
+	"path"
+	"path/filepath"
+	"rest-api/internal/config"
+	"rest-api/internal/user"
+	"rest-api/pkg/logging"
 	"time"
 
 	"github.com/julienschmidt/httprouter"
@@ -15,20 +20,41 @@ func main() {
 	logger.Info("creat router")
 	router := httprouter.New()
 
+	cfg := config.GetConfig()
 	logger.Info("register user handler")
 	handler := user.NewHandler(logger)
 	handler.Register(router)
 
-	start(router)
+	start(router, cfg)
 }
 
-func start(router *httprouter.Router) {
+func start(router *httprouter.Router, cfg *config.Config) {
 	logger := logging.GetLogger()
-
 	logger.Info("start app")
-	listener, err := net.Listen("tcp", ":1234")
-	if err != nil {
-		panic(err)
+
+	var listener net.Listener
+	var listenErr error
+
+	if cfg.Listen.Type == "sock" {
+		logger.Info("detect app path")
+		appDir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+		if err != nil {
+			logger.Fatal(err)
+		}
+		logger.Info("create socket")
+		socketPath := path.Join(appDir, "app.sock")
+
+		logger.Info("listen unix socket")
+		listener, listenErr = net.Listen("unix", socketPath)
+		logger.Infof("server listening unix socket: %s", socketPath)
+	} else {
+		logger.Info("listen tcp")
+		listener, listenErr = net.Listen("tcp", fmt.Sprintf("%s:%s", cfg.Listen.BindIp, cfg.Listen.Port))
+		logger.Infof("server listening port %s:%s", cfg.Listen.BindIp, cfg.Listen.Port)
+	}
+
+	if listenErr != nil {
+		logger.Fatal(listenErr)
 	}
 
 	server := &http.Server{
@@ -37,6 +63,5 @@ func start(router *httprouter.Router) {
 		ReadTimeout:  15 * time.Second,
 	}
 
-	logger.Info("server listening port 0.0.0.0:1234")
 	logger.Fatal(server.Serve(listener))
 }
---
+++ internal/handlers/handler.go
@@ -0,0 +1,8 @@
+package handlers
+
+import "github.com/julienschmidt/httprouter"
+
+type Handler interface {
+	Register(router *httprouter.Router)
+}
+
